2b70bd9194abc59d5a6a7278eb4e38aa27e1cce8
Dee Catsey <deecatsey@gmail.com>
2026-07-23 22:51:39 +0200

    bar chart

--- src/components/chart/VotesChart.tsx
+++ src/components/chart/VotesChart.tsx
@@ -2,7 +2,9 @@ import {
   Bar,
   BarChart,
   CartesianGrid,
+  Cell,
   Legend,
+  Rectangle,
   Tooltip,
   XAxis,
   YAxis,
@@ -16,22 +18,6 @@ import { getColor } from "../../utils/get-party-color";
 
 type VotesChartProps = { feature: ElectionFeature };
 
-// const data = [
-//   { name: "LINZ", votes: 15 },
-//   { name: "SPÖ", votes: 33 },
-// ];
-
-// const data = [{ LINZ: 15, SPÖ: 33 }];
-
-const testdata = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-];
-
 export const VotesChart = ({ feature }: VotesChartProps) => {
   // if (!feature) return null;
 
@@ -42,16 +28,6 @@ export const VotesChart = ({ feature }: VotesChartProps) => {
     },
   } = feature;
 
-  console.log(
-    "???? WTF?votes",
-    feature.feature.properties.votes,
-    "DESTRUCT",
-    votes,
-  );
-
-  const votesData = Object.values(PartiesConsts).map((party) => {
-    return votes[party.identifier];
-  });
   const getResults = (data: Record<string, number>, parties: string[]) =>
     Object.fromEntries(
       parties.map((p) => [
@@ -72,25 +48,8 @@ export const VotesChart = ({ feature }: VotesChartProps) => {
   // const votes = feature.feature.properties.votes;
   // if (!votes) return null;
 
-  const bars = Object.values(PartiesConsts).map((party) => (
-    <Bar
-      key={`${party.identifier}-bar`}
-      dataKey={party.identifier}
-      fill={getColor(party.identifier)}
-      // activeBar={{ fill: getColor("SPÖ"), stroke: "blue" }}
-      radius={[10, 10, 0, 0]}
-    />
-  ));
-
-  const mehData = Object.entries(voteData)
-    .map(([name, votes]) => ({
-      name,
-      votes,
-    }))
-    .sort((a, b) => b.votes - a.votes);
-
   return (
-    <div className="bar-chart-container" style={{ height: "300px" }}>
+    <div className="bar-chart-container">
       <BarChart
         style={{
           width: "100%",
@@ -110,35 +69,19 @@ export const VotesChart = ({ feature }: VotesChartProps) => {
         <XAxis dataKey="name" />
         <YAxis width="auto" />
         <Tooltip />
-        <Legend />
         <Bar
           dataKey="votes"
-          fill="#8884d8"
-          activeBar={{ fill: "pink", stroke: "blue" }}
-          radius={[10, 10, 0, 0]}
+          radius={[5, 5, 0, 0]}
+          shape={(props) => {
+            return (
+              <Rectangle
+                {...props}
+                fill={getColor(props.payload.name) ?? "#999999"}
+              />
+            );
+          }}
         />
       </BarChart>
-      {/* <BarChart
-        style={{
-          width: "100%",
-          maxWidth: "700px",
-          maxHeight: "70vh",
-          aspectRatio: 1.618,
-        }}
-        responsive
-        data={[chartData]}
-        margin={{
-          top: 5,
-          right: 0,
-          left: 0,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <Legend />
-        <Tooltip />
-        {bars}
-      </BarChart> */}
     </div>
   );
 };
